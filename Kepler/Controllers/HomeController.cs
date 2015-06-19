83df93f4e0c82fb170b2ad993e60c9a79b5f69fd
Nikita Kudrin <nikita.kudrin@sravni.ru>
2015-06-20 00:37:12 +0300

    Project Init

---
+++ Kepler/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+
+namespace Kepler.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Kepler";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Status()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
