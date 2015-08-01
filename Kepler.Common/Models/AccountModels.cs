58d48a422416246c1e8e535d556deeb379365f0f
Nikita Kudrin <nikita.kudrin@sravni.ru>
2015-08-02 01:51:15 +0300

    * split project into different libs

--- Kepler.Common/Models/AccountModels.cs
+++ Kepler.Common/Models/AccountModels.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Globalization;
-using System.Web.Security;
+
 
 namespace Kepler.Models
 {
