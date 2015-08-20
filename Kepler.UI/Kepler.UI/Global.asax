3ad94dc38984b40d94f9c6be350e46f092af40e5
walsash <walsashka@gmail.com>
2015-08-21 00:40:42 +0600

    New Kepler.UI

---
+++ Kepler.UI/Kepler.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kepler.UI.MvcApplication" Language="C#" %>
---
+++ Kepler.UI/Kepler.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kepler.UI.Startup))]
+namespace Kepler.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
