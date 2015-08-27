2fc2c9c23261ceb1bbe411201031f31721eebe47
Nikita Kudrin <nikita.kudrin@sravni.ru>
2015-08-27 22:57:23 +0300

    API: GetBuild, GetBuilds

--- Kepler.Common/Models/Build.cs
+++ Kepler.Common/Models/Build.cs
@@ -1,9 +1,23 @@
-﻿using System.Data.Entity.Core.Objects;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 
 namespace Kepler.Core
 {
     public class Build : InfoObject
     {
+        [DataMember]
+        [DataType(DataType.DateTime)]
+        public DateTime? StartDate { get; set; }
+
+        [DataMember]
+        [DataType(DataType.Time)]
+        public TimeSpan? Duration { get; set; }
+
+        [DataMember]
+        public long TestCount { get; set; }
+
+        [DataMember]
+        public long TestsFailed { get; set; }
     }
 }
--- Kepler.Service/KeplerService.cs
+++ Kepler.Service/KeplerService.cs
@@ -1,36 +1,21 @@
 ﻿using System;
-using System.ServiceModel.Activation;
+using System.Collections.Generic;
 using Kepler.Core;
 
 namespace Kepler.Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-   // [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class KeplerService : IKeplerService
     {
+        private BuildRepository buildRepo = BuildRepository.Instance;
+
         public Build GetBuild(string id)
         {
-            var repo = BuildRepository.Instance;
-
-            return repo.Get(Convert.ToInt64(id));
+            return buildRepo.Get(Convert.ToInt64(id));
         }
 
-        /* public IEnumerable<Build> GetBuilds()
+        public IEnumerable<Build> GetBuilds()
         {
-            throw new NotImplementedException();
+            return buildRepo.FindAll();
         }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }*/
     }
 }
