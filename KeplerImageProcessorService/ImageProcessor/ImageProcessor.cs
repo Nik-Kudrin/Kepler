45ee9070c1289404b280515e2d6f70c9e3bb266f
Nikita Kudrin <nikita.kudrin@sravni.ru>
2015-09-17 00:35:39 +0300

    Some example with Magick.Net

--- KeplerImageProcessorService/ImageProcessor/ImageProcessor.cs
+++ KeplerImageProcessorService/ImageProcessor/ImageProcessor.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ImageMagick;
 
 namespace KeplerImageProcessorService.ImageProcessor
 {
     public class ImageProcessor
     {
+        public bool GetImageDiff(string firstImagePath, string secondImagePath, string diffImgPathToSave)
+        {
+            using (var firstImage = new MagickImage(firstImagePath))
+            using (var secondImage = new MagickImage(secondImagePath))
+            using (var diffImage = new MagickImage())
+            {
+                if (firstImage.GetHashCode() == secondImage.GetHashCode())
+                    return true;
+
+                firstImage.Compare(secondImage, ErrorMetric.PeakSignalToNoiseRatio, diffImage);
+                diffImage.Write(diffImgPathToSave);
+                return false;
+            }
+        }
     }
 }
