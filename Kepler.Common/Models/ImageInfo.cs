86360ceabfa3567f98493b1d1d152d1681228609
Nikita Kudrin <nikita.kudrin@sravni.ru>
2015-09-18 20:15:31 +0300

    image diff task generator. Incompleted

---
+++ Kepler.Common/Models/ImageInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Kepler.Common.Models
+{
+    public class ImageInfo
+    {
+        public long ScreenShotId { get; set; }
+        public bool IsImagesDifferent { get; set; }
+
+        public string FirstImagePath { get; set; }
+        public string SecondImagePath { get; set; }
+        public string DiffImgPathToSave { get; set; }
+    }
+}
---
+++ KeplerImageProcessorService/ImgProcessor/ImageProcessor.cs
@@ -0,0 +1,54 @@
+﻿using ImageMagick;
+using Kepler.Common.Models;
+
+namespace KeplerImageProcessorService.ImgProcessor
+{
+    public class ImageProcessor
+    {
+        public ImageInfo ImageInfo { get; set; }
+
+        public ImageProcessor()
+        {
+        }
+
+        public ImageProcessor(ImageInfo imageInfo)
+        {
+            this.ImageInfo = imageInfo;
+        }
+
+
+        public ImageInfo GetCompositeImageDiff()
+        {
+            using (var firstImage = new MagickImage(ImageInfo.FirstImagePath))
+            using (var secondImage = new MagickImage(ImageInfo.SecondImagePath))
+            {
+                ImageInfo.IsImagesDifferent = false;
+
+                if (firstImage.GetHashCode() == secondImage.GetHashCode())
+                    return ImageInfo;
+
+                firstImage.Composite(secondImage, CompositeOperator.Difference);
+                firstImage.Write(ImageInfo.DiffImgPathToSave);
+                ImageInfo.IsImagesDifferent = true;
+                return ImageInfo;
+            }
+        }
+
+        public ImageInfo GetCompareImageDiff()
+        {
+            using (var firstImage = new MagickImage(ImageInfo.FirstImagePath))
+            using (var secondImage = new MagickImage(ImageInfo.SecondImagePath))
+            using (var diffImage = new MagickImage())
+            {
+                ImageInfo.IsImagesDifferent = false;
+
+                if (firstImage.GetHashCode() == secondImage.GetHashCode())
+                    return ImageInfo;
+
+                firstImage.Compare(secondImage, ErrorMetric.Absolute, diffImage, Channels.Index);
+                diffImage.Write(ImageInfo.DiffImgPathToSave);
+                return ImageInfo;
+            }
+        }
+    }
+}
