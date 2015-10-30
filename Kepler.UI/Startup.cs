4dcc88c00d31bdc5fdddd3eb867773bbb74a70ff
Nikita Kudrin <nikita.kudrin@sravni.ru>
2015-10-30 21:43:07 +0300

    Init kepler.service url one time when site startup

--- Kepler.UI/Startup.cs
+++ Kepler.UI/Startup.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(Kepler.UI.Startup))]
+[assembly: OwinStartupAttribute(typeof (Kepler.UI.Startup))]
+
 namespace Kepler.UI
 {
     public partial class Startup
     {
+        public static string KeplerServiceUrl = "";
+
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            KeplerServiceUrl = System.Web.Configuration.WebConfigurationManager.AppSettings["Kepler.Service"];
         }
     }
 }
