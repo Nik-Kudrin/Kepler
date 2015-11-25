c12e8bd5bde80ab9429f32bfa3b3bd0d97a20600
Nikita Kudrin <nikita.kudrin@sravni.ru>
2015-11-25 19:33:33 +0300

    Removed unnecessary routing

--- Kepler.UI/App_Start/RouteConfig.cs
+++ Kepler.UI/App_Start/RouteConfig.cs
@@ -8,17 +8,12 @@ namespace Kepler.UI
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            
+
             routes.MapRoute(
                 name: "Default",
                 url: "{*url}",
                 defaults: new {controller = "Kepler", action = "Index"}
                 );
-           /* routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new {controller = "Kepler", action = "Index", id = UrlParameter.Optional}
-                );*/
         }
 
         protected void Application_Start()
