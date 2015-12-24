373e126964a0c029ebe3ff41cc6ddcad8d9dc97c
Nikita Kudrin <nikita.kudrin@sravni.ru>
2015-12-25 00:04:23 +0300

    Remove WPF application. Created Win Forms custom application - installer

---
+++ Kepler.Service.Setup/MainForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Kepler.Service.Setup
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InstallButton = new System.Windows.Forms.Button();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.installationPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Specify directory for installation Kepler Service";
+            // 
+            // InstallButton
+            // 
+            this.InstallButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.InstallButton.Location = new System.Drawing.Point(367, 201);
+            this.InstallButton.Name = "InstallButton";
+            this.InstallButton.Size = new System.Drawing.Size(75, 23);
+            this.InstallButton.TabIndex = 3;
+            this.InstallButton.Text = "Install";
+            this.InstallButton.UseVisualStyleBackColor = true;
+            this.InstallButton.Click += new System.EventHandler(this.InstallButton_Click);
+            // 
+            // browseButton
+            // 
+            this.browseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.browseButton.Location = new System.Drawing.Point(367, 54);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // installationPath
+            // 
+            this.installationPath.Location = new System.Drawing.Point(12, 54);
+            this.installationPath.Name = "installationPath";
+            this.installationPath.Size = new System.Drawing.Size(349, 20);
+            this.installationPath.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 236);
+            this.Controls.Add(this.InstallButton);
+            this.Controls.Add(this.installationPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.browseButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kepler Service Installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button InstallButton;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox installationPath;
+    }
+}
+
---
+++ Kepler.Service.Setup/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Security.AccessControl;
+using System.Windows.Forms;
+
+namespace Kepler.Service.Setup
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InstallButton_Click(object sender, System.EventArgs e)
+        {
+            if (installationPath.Text.Trim() == "")
+            {
+                MessageBox.Show("Please, specify installation directory", "Empty installation path",
+                    MessageBoxButtons.OK);
+                return;
+            }
+
+            if (!Directory.Exists(installationPath.Text))
+                Directory.CreateDirectory(installationPath.Text);
+        }
+
+        private void browseButton_Click(object sender, System.EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+            installationPath.Text = folderBrowserDialog.SelectedPath;
+        }
+    }
+}
